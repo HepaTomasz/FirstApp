7f11fd6b38872ac89fa046ae9a8683b10659f040
Tomasz Hepa <tomasz.hepa@exozet.com>
2018-02-05 19:18:09 +0100

    Updated MyGame VC

---
+++ FIRSTAPP/FIRSTAPP/DropView.swift
@@ -0,0 +1,64 @@
+//
+//  DropView.swift
+//  FIRSTAPP
+//
+//  Created by Tomasz Hepa on 05.02.18.
+//  Copyright © 2018 Tomasz Hepa. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class DropView: UIImageView {
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+     
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self)
+            // do something with your currentPoint
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self)
+            // do something with your currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self)
+            // do something with your currentPoint
+            
+            print(currentPoint)
+        }
+    }
+
+}
+
+
--- FIRSTAPP/FIRSTAPP/MyGame.swift
+++ FIRSTAPP/FIRSTAPP/MyGame.swift
@@ -8,31 +8,42 @@
 
 import UIKit
 
+
 class MyGame: UIViewController {
 
+    @IBOutlet weak var gameOverTxtLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+
+    var score = 0
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
     
     var drops : [UIImageView] = []
-    let countDrops = 10
-    
+    let countDrops = 15
+    let dropSize : CGFloat = 40
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
         // Do any additional setup after loading the view.
+        // recognizer to get touch events on animated views
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.removeDrop(_:)))
+        self.view.addGestureRecognizer(tapRecognizer)
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func startBtnTouched(_ sender: Any) {
+        
+        // creating drops
         startButton.isHidden = true
         let width = self.view.frame.width
         let height = self.view.frame.height
-        let dropSize : CGFloat = 40
         var randXBeginArr : Array<CGFloat> = []
         var randXEndArr : Array<Int> = []
         var durations : Array<CGFloat> = []
@@ -42,26 +53,67 @@ class MyGame: UIViewController {
             self.drops.append(UIImageView(image: #imageLiteral(resourceName: "rainDrop")))
             randXBeginArr.append(CGFloat(arc4random_uniform(UInt32(width))))
             randXEndArr.append(Int(arc4random_uniform(UInt32(width))))
-            durations.append(randomCGFloat(min: 0.5, max: 5.0))
+            durations.append(randomCGFloat(min: 5, max: 17.0))
             animDelayArr.append(randomCGFloat(min: 0.0, max: 10))
             
             self.drops[index].frame = CGRect(x: randXBeginArr[index], y: -dropSize, width: dropSize, height: dropSize)
-            
+            if drops[index].frame == CGRect(x: randXBeginArr[index], y:0, width: dropSize, height: dropSize){
+                gameOverTxtLabel.isHidden = false
+                
+            }
+            //self.drops[index].isUserInteractionEnabled = true
             self.view.addSubview(self.drops[index])
-            
+            // animate the drop
             UIView.animate(withDuration: TimeInterval(durations[index]), delay: TimeInterval(animDelayArr[index]), options: .repeat, animations: {
-                self.drops[index].center = CGPoint.init(x: randXBeginArr[index], y: height + dropSize)
+                self.drops[index].center = CGPoint.init(x: randXBeginArr[index], y: height + self.dropSize)
             }, completion: nil)
-            
         }
+        runTimer()
+    }
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(MyGame.updateTimer)), userInfo: nil, repeats: true)
     }
     
+    @objc func removeDrop(_ sender: UITapGestureRecognizer)
+    {
+        for index in 0..<self.drops.count {
+            let point = sender.location(in: self.view)
+            let presentationPosition = self.drops[index].layer.presentation()?.position
+            
+            if (point.x > presentationPosition!.x - self.dropSize/2 && point.x < presentationPosition!.x + self.dropSize/2
+                && point.y > presentationPosition!.y - self.dropSize/2 && point.y < presentationPosition!.y + self.dropSize/2)
+            {
+                print("inside view")
+                score += 1
+                // remove the drop from view and from the drops array
+                self.drops[index].removeFromSuperview()
+                self.drops.remove(at: index)
+                pointsLabel.text = "\(score)"
+                
+                break;
+                // maybe later a nice explosion animation
+            }
+            else{
+                print("outside view")
+            }
+
+        }
+    }
+    // some helper funtions
     
-    
+    @objc func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = "\(seconds)"  // This will update the label.
+    }
     
     func randomCGFloat(min: CGFloat, max: CGFloat) -> CGFloat {
         return CGFloat(Float(arc4random()) / Float(UINT32_MAX)) * (max - min) + min
     }
+    @IBAction func restartBtnTouched(_ sender: Any) {
+        loadView()
+    }
+    
+   
     
     /*
     // MARK: - Navigation
